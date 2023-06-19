f39a5bc4fbfeb9279268c404542cb7463b606015
Kevin Wijesooriya <kevin@hatchyard.io>
2023-06-19 14:53:45 +0530

    refactors

--- client/src/Screens/Appointments/MakeAppointment.jsx
+++ client/src/Screens/Appointments/MakeAppointment.jsx
@@ -1,6 +1,5 @@
 import { Box, Button, Grid, Paper, TextField, Typography } from "@mui/material";
 import React from "react";
-import { Link } from "react-router-dom";
 
 const MakeAppointment = () => {
   return (
